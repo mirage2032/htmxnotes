41ccd090b9a09251ed954b24611e7f363f67e5b9
Ionut <pialex2032@gmail.com>
2024-04-01 22:05:06 +0300

    update

--- db/initdb/03notes.sql
+++ db/initdb/03notes.sql
@@ -45,11 +45,27 @@ CREATE TRIGGER note_group_update_trigger
     FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_column();
 
-CREATE TABLE note_member_groups
+CREATE TABLE note_group_permissions
+(
+    id              SERIAL PRIMARY KEY,
+    note_id         INTEGER NOT NULL REFERENCES notes(id),
+    user_id         INTEGER NOT NULL REFERENCES users(id),
+    group_id        INTEGER NOT NULL REFERENCES user_groups(id),
+    permission      note_user_role NOT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT check_note_user_exclusive_ngp CHECK (
+        (user_id IS NOT NULL AND group_id IS NULL) OR
+        (user_id IS NULL AND group_id IS NOT NULL)
+        ),
+    CONSTRAINT unique_user_note_combination_ngp UNIQUE (note_id, user_id),
+    CONSTRAINT unique_group_note_combination_ngp UNIQUE (note_id, group_id)
+);
+
+CREATE TABLE note_group_members
 (
     id              SERIAL PRIMARY KEY,
     note_id         INTEGER NOT NULL REFERENCES notes(id),
     group_id        INTEGER NOT NULL REFERENCES note_groups(id),
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    CONSTRAINT unique_note_group_combination_nmg UNIQUE (note_id, group_id)
+    CONSTRAINT unique_note_group_combination_ngm UNIQUE (note_id, group_id)
 );
